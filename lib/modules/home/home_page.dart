0d71002a9d56836a467ac9bcde0cc68f3275c64a
Abner Silva <abner.computadores@outlook.com>
2021-05-26 16:24:11 -0300

    fazendo redirecionamento de pagina quando clica nos respectivos botões

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -7,9 +7,25 @@ class HomePage extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomePage'),
+        title: Text('Home Page'),
       ),
-      body: Container(),
+      body: Center(
+        child: Wrap(
+          spacing: 10,
+          children: [
+            ElevatedButton.icon(
+              onPressed: () => Get.toNamed('/movies', arguments: 'Populares'),
+              icon: Icon(Icons.pets_outlined),
+              label: Text('Populares'),
+            ),
+            ElevatedButton.icon(
+              onPressed: () => Get.toNamed('/movies', arguments: 'Mais Votados'),
+              icon: Icon(Icons.pets_outlined),
+              label: Text('Mais votados'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
