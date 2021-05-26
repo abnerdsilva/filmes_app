bd3b2380da2651d12e1b6992e3a0aa1671c6d58c
Abner Silva <abner.computadores@outlook.com>
2021-05-26 20:35:14 -0300

    iniciando leitura de arquivo .env

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:filmes_app/modules/home/home_page.dart';
 import 'package:filmes_app/modules/movies/movies_bindings.dart';
 import 'package:filmes_app/modules/movies/movies_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
 void main() {
+  load();
+
   runApp(MyApp());
 }
 
